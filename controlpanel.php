e72ff35a58bf2a6be31a06446a143fd8a1a755db
Alfonso De La Guardia <alfonsodelag1@gmail.com>
2020-01-07 15:56:13 +0100

    add php files

---
+++ controlpanel.php
@@ -0,0 +1,74 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Control Panel</title>
+</head>
+
+<body>
+
+<h1>Username: Alfonso De La Guardia</h1>
+
+<div class="row">
+    <div class="col">
+    <p>List Posts</p>
+    </div>
+</div> 
+
+<div class="row">
+    <div class="col">
+    <p>Edit Post</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Delete Post</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Create Post</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Delete Comment</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Create Category</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Edit Category</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>Delete Category</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col">
+    <p>List Category</p>
+    </div>
+</div>
+
+<button>Logout</button>
+
+</body>
+</html>
